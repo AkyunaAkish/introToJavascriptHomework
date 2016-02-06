b5c81313b3227ab6b4cc4ab65b9a3de3baf61247
bud aminof <budaminof@gmail.com>
2016-02-06 16:03:22 -0700

    Math.random()- gives you a random float number between 0 to 1, not including 1

--- stretch.js
+++ stretch.js
@@ -3,8 +3,20 @@
 //if its less than 0.5 it will round it to 0
 //so its basically decimal rounding..
 
-x = Math.round(37.85);
+var x = Math.round(37.85);
 console.log(x);
 
-y = Math.round(43.23);
+var y = Math.round(43.23);
 console.log(y);
+
+//Math.random() gives a random floating number between (including) 0 up to (not including) 1.
+
+function getRandom(){
+  return Math.random();
+};
+console.log(getRandom());
+
+function getNum() {
+  return Math.floor(Math.random());
+};
+console.log(getNum());

480810263e426a7f221965906bb0e7d880e6c0ae
bud aminof <budaminof@gmail.com>
2016-02-06 16:13:19 -0700

    Math.min and Math.max- gives you the smallest or the biggest number

--- stretch.js
+++ stretch.js
@@ -20,3 +20,26 @@ function getNum() {
   return Math.floor(Math.random());
 };
 console.log(getNum());
+
+//Math.min() function return the smallest of zero or more number
+// if no argumnets given it will return Infinity
+// if one argument is not a number it will return Nan
+
+var x = 2
+var i = 10
+var y = 8.7
+var z = Math.min(x, i, y);
+console.log(z);
+
+
+//Math.max() function returns the largest of zero or more numbers
+// if no argumnets given it will return Infinity
+// if one argument is not a number it will return Nan
+//! this is what I should have used for the application challange!!
+
+var i = Math.max(3, 67, 8.9, 0.33);
+console.log(i);
+
+var arr = [1, 3, 5, 7];
+var big = Math.max(...arr);
+console.log(big);

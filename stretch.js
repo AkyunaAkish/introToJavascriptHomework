9c9569efd2312a492ebf03f6b796a9900fc5e592
bud aminof <budaminof@gmail.com>
2016-02-06 15:55:53 -0700

    Math.round()-decimal rounding

---
+++ stretch.js
@@ -0,0 +1,10 @@
+// Math.round()
+//if the fractional portion of number if 0.5 or bigger it will round it up
+//if its less than 0.5 it will round it to 0
+//so its basically decimal rounding..
+
+x = Math.round(37.85);
+console.log(x);
+
+y = Math.round(43.23);
+console.log(y);

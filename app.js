c287b9fad72bbdd0ede9745de7dcc49227a838e6
Seth Lytton <slytton@gmail.com>
2016-02-06 16:31:16 -0700

    Add math functions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(c);
 
 
 var example = 1 + 2;
-console.log("example should equal: 1, does equal: " + example);
+console.log("example should equal: 3, does equal: " + example);
 
 
 example = 1 - 2;
@@ -26,3 +26,21 @@ console.log("example should equal: 1, does equal: " + example);
 
 example = -4;
 console.log("example should equal: -4, does equal: " + example);
+
+example = Math.round(4.35);
+console.log("round(4.35) should equal: 4, does equal: " + example);
+
+example = Math.random();
+console.log("should equal: a number between zero and 1, does equal: " + example);
+
+example = Math.min(2,5);
+console.log("min(2,5) should equal: 2, does equal: " + example);
+
+example = Math.max(2,5);
+console.log("max(2,5) should equal: 5, does equal: " + example);
+
+example = Math.floor(1.99999);
+console.log("floor(1.9999) should equal: 1, does equal: " + example);
+
+example = Math.ceil(1.00001);
+console.log("floor(1.00001) should equal: 2, does equal: " + example);

15e72e8eb88f510a9732d9f09f13fef2997d90c0
Seth Lytton <slytton@gmail.com>
2016-02-05 19:20:53 -0700

    Add arithmetic operators to app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+
+var example = 1 + 2;
+console.log("example should equal: 1, does equal: " + example);
+
+
+example = 1 - 2;
+console.log("example should equal: -1, does equal: " + example);
+
+example = 1 / 2;
+console.log("example should equal: .5, does equal: " + example);
+
+example = 25 % 2;
+console.log("example should equal: 1, does equal: " + example);
+
+example++;
+console.log("example should equal: 2, does equal: " + example);
+
+example--;
+console.log("example should equal: 1, does equal: " + example);
+
+example = -4;
+console.log("example should equal: -4, does equal: " + example);

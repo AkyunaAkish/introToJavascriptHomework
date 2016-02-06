195ce5527ffdd3a3654dac6a66f1c31712d42749
Courtney Morrissey <courtneycmorrissey@gmail.com>
2016-02-06 11:13:28 -0700

    ran js file with aritmetic examples

---
+++ app.js
@@ -0,0 +1,34 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+var d = 34;
+var e = 6;
+var f = ((d - e)*d)/e;
+
+console.log(f);
+
+var g = d % e;
+ console.log(g);
+
+var h = e++;
+var i = ++e;
+
+console.log(h);
+console.log(i);
+
+var j = e--;
+var k = --e;
+
+console.log(j);
+console.log(k);
+
+var l = -e;
+
+console.log(l);
+
+var m = +e;
+
+console.log(m);

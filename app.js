5635038eb2283be3d85c3082165848699d8b8985
Alan Taylor <alancarltaylor@gmail.com>
2016-02-07 12:33:52 -0700

    I now have an example of modulus

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ console.log(d); //this executes simple subtraction
 //exaple 3, '/'
 var e = 567/36.5;
 console.log(e); //this executes division
+
+//example 4, '%'
+var f = 46%5;
+console.log(f);//this gives the remainder of the division of these two numbers

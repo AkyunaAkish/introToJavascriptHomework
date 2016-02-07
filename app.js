96111ba6299f16a8714f3c34a76073fd707b740a
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-06 19:12:01 -0700

    Test '/' operator

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ var c = a + b;
 
 console.log(c);
 
-//The mathematical operator '-' os how javascript subtracts values
+//The mathematical operator '-' is how javascript subtracts values
 
 var a = 22;
 var b = 10;
 var c = a - b;
+
+console.log(c);
+
+//The mathematical operator '/' is how javascript dividess values
+//Includes remainder as decimal
+
+var a = 22;
+var c = a / b;
+var b = 10;
 
 console.log(c);

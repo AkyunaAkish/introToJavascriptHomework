b082de8445ea4ef12b098581c661819d02a06d8b
Summer Wollin <summer.wollin@gmail.com>
2016-02-07 14:30:10 -0700

    multiplication operator in js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var c = a + b;
 console.log(c);
 
 // The mathematical operator '+' is how javascript adds two values
-var example = 4 + 2;
-console.log(example);
+var additionExample = 4 + 2;
+console.log(additionExample);
 
 //The mathematical operator '-' is how javascript subtracts values
 var subtractionExample = 4 - 1;
@@ -15,3 +15,7 @@ console.log(subtractionExample);
 //The mathematical operator '/' is how javascript divides values
 var divisionExample = 10 / 2;
 console.log(divisionExample);
+
+//The mathematical operator '*' is how javascript multiplies values
+var multiplicationExample = 10 * 10;
+console.log(multiplicationExample);

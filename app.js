ce9eb5fa4c4d5bef81bc5fc42d6984ba04b8c156
MSturges <Sturges3232@gmail.com>
2016-02-07 15:41:53 -0700

    solution

---
+++ app.js
@@ -0,0 +1,37 @@
+// addition
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+
+// subtraction
+var Chicken = 10;
+var Tuna = 30;
+var answerToLife = Tuna - Chicken;
+
+console.log(answerToLife);
+
+// Division
+
+var ohSnap = 36;
+var hellNo = 6;
+var ohSnapandHellno = hellNo / ohSnap;
+
+console.log(ohSnapandHellno);
+
+// Multiplication
+
+var dog = 1;
+var cat = 1;
+var catdog = dog * cat;
+
+console.log(catdog);
+
+// Remaninder
+var left = 10;
+var right = 25;
+var together = right % left;
+
+console.log(together);

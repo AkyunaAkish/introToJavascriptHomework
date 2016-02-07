aa5e6a1be616c0a14fad9181f83ad282be1ac23e
Alan Taylor <alancarltaylor@gmail.com>
2016-02-07 12:32:29 -0700

    I now have an example of division

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ console.log(c); //this executes simple addition
 //example 2, '-'
 var d = 5 - 6;
 console.log(d); //this executes simple subtraction
+
+//exaple 3, '/'
+var e = 567/36.5;
+console.log(e); //this executes division

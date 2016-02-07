35bef9cc8c461f2af936c9daa91f95d6ec5a85ab
Madeline Pickering <madelinepickering@gmail.com>
2016-02-07 11:10:45 -0700

    operators explained

---
+++ app.js
@@ -0,0 +1,41 @@
+// The mathematical operator '+' is how javascript adds two values together
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+// operator '-' subtracts the second value from the first
+var minus = 4 - 2;
+console.log(minus);
+
+// operator '/' divides the first value by the second
+var divide = 20/2;
+console.log(divide);
+
+// operator '*' multiples two values
+var mult = 3*3;
+console.log(mult);
+
+// operator '%' provides the remainder when value one is divided by value two
+var perc = 10 % 3;
+console.log(perc);
+
+// operator '++' increments a variable by one
+var incr = 3;
+y = ++incr;
+console.log(y);
+
+// operator '--' decreases a variable by one
+var decr = 8;
+ement = --decr;
+console.log(ement);
+
+// operator '-' ahead of a variable makes it negative
+var j = 7;
+h = -j;
+console.log(h);
+
+// operator '+' ahead of a variable attempts to convert it to a number
+var strang = "7"
+console.log(+strang);

6740e0e34b46d5497cfd0bbef54cdda0e142b87e
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-06 19:29:55 -0700

    Test '++' operator

--- app.js
+++ app.js
@@ -38,3 +38,18 @@ var c = a % b;
 var b = 10;
 
 console.log(c);
+
+//The mathematical operator '++' is how javascript one is added to a value
+//'++' before the identifier '1' is added to the original identifier while and value is returned 
+
+var a = 22;
+var c = ++a;
+
+console.log(a,c);
+
+//'++' after the indentifier '1' is added to the original identifier, however, the original value is returned
+
+var a = 22;
+var c = a++;
+
+console.log(a,c);

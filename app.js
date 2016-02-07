5f849ff97949cb0178131d2f20f7c56339a06b95
Mike Homolak <mhomolak@yahoo.com>
2016-02-07 14:44:14 -0800

    A/S/M/D

---
+++ app.js
@@ -0,0 +1,17 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+// Adding then outputting with node
+var addition = 13 + 3;
+console.log(addition);
+// Subtracting then outputting with node
+var subtraction = 3284 - 1482;
+console.log(subtraction);
+// Multiplying then outputting with node
+var mult = 32 * 17;
+console.log(mult);
+// Dividing then outputting with node
+var divide = 483 / 13;
+console.log(divide);

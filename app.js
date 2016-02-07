49684611084b4d63d6cb1b920b2171ff6854afba
Wfuger <wfuger@gmail.com>
2016-02-06 23:10:07 -0700

    Comment, variable declaration & assignment, and logging to console for subtraction operator

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ console.log(c);
 // The operator '+' is how javascript adds values together
 var addition = 10 + 10;
 console.log(addition);
+
+// The operator '-' is how javascript subtracts two values giving you the difference
+var subtraction = 99 - 39;
+console.log(subtraction);
+
+// 

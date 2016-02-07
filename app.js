3321c5c6d18da1a0e806a3c493a6210b936614e3
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-06 19:07:55 -0700

    Test '+' and '-' operators

---
+++ app.js
@@ -0,0 +1,15 @@
+//The mathematical operator '+' is how javascript adds values together
+
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+//The mathematical operator '-' os how javascript subtracts values
+
+var a = 22;
+var b = 10;
+var c = a - b;
+
+console.log(c);

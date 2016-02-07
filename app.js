e73e06f415c9f17f85067e496e4118627f8ba566
Summer Wollin <summer.wollin@gmail.com>
2016-02-07 14:24:50 -0700

    addition operator in js

---
+++ app.js
@@ -0,0 +1,9 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+// The mathematical operator '+' is how javascript adds two values together
+var example = 4 + 2;
+console.log(example);

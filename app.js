62f839215f55dd3a33e80d798dddd3fcf61fddd3
Summer Wollin <summer.wollin@gmail.com>
2016-02-07 14:27:29 -0700

    subtraction opperator in js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var c = a + b;
 
 console.log(c);
 
-// The mathematical operator '+' is how javascript adds two values together
+// The mathematical operator '+' is how javascript adds two values
 var example = 4 + 2;
 console.log(example);
+
+//The mathematical operator '-' is how javascript subtracts values
+var subtractionExample = 4 - 1;
+console.log(subtractionExample);

4674db437456f24812d32d91335cb2e0e840b081
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-07 10:19:54 -0700

    Add + and - operators

---
+++ app.js
@@ -0,0 +1,13 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+// Operator '+' adds to values together (when used with numbers)
+var addEx = 3 + 4;
+console.log(addEx);
+
+// Operator '-' subtracts the second value from the first
+var subEx = 8-5;
+console.log(subEx);

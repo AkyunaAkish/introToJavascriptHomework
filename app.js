b13666b524d53ca8d811ace429ece495c4c917d3
Alan Taylor <alancarltaylor@gmail.com>
2016-02-07 12:29:22 -0700

    I now have examples of subtraction

---
+++ app.js
@@ -0,0 +1,11 @@
+
+//example 1, '+'
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c); //this executes simple addition
+
+//example 2, '-'
+var d = 5 - 6;
+console.log(d); //this executes simple subtraction

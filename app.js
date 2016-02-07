5805d7d7127f80f9784e081087acb962ef4baf64
Mike Homolak <mhomolak@yahoo.com>
2016-02-07 15:00:47 -0800

    Stretch goal

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ console.log(mult);
 // Dividing then outputting with node
 var divide = 483 / 13;
 console.log(divide);
+//Math.round rounds to the nearest integer
+var d = Math.round(23.41);
+console.log(d);
+//Math.random returns a number from 0 up to, but not including, 1
+var e = Math.random();
+console.log(e);
+//Math.min returns the smallest number based on the parameters given
+var f = Math.min(1, 3, 5);
+console.log(f);
+//Math.max returns the largest number based on the parameters given
+var g = Math.max(1, 3, 5, 7);
+console.log(g);
+//Math.floor returns the largest integer less than or equal to a given number.
+var h = Math.floor(43.12);
+console.log(h);
+//Math.ceil returns the smallest integer greater than or equal to a given number.
+var i = Math.ceil(43.12);
+console.log(i);

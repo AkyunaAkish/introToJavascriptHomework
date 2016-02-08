6aa26e777b58938b086d7aaf39d8090d26dad076
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-08 07:01:58 -0700

    js with additional arithmetic operators

--- app.js
+++ app.js
@@ -48,3 +48,73 @@ var cc = aa % bb;
 console.log(cc);
 // The mathematical operator '%' (modulo )is how javascript finds the integer remainder
 // of two values
+
+var dd = 5
+var ee = -dd
+
+console.log(ee);
+// The mathematical operator '-' is can also be called 'unary negation' and negates  an integer.
+// or makes positive a negative integer
+
+ff = true
+
+console.log(+ff);
+// The mathematical operator '+'can also be called unary plus and can turn an number
+// or a string or even a boolean into a number. (+true evaluates to 1)
+
+var ab = 5.7
+
+console.log(Math.round(ab));
+// The mathematical operator 'Math.round' is how javascript uses normal rounding
+// rules, round up if >= 0.5 and down if < 0.5  to the nearest integer
+
+var ba = 5.7
+
+console.log(Math.floor(ba));
+// The mathematical operator 'Math.floor' will always round a decimal down to
+//the nearest whole integer actually to the nearest decicmal if there is more
+//than one decimal place
+
+var bc = 9.1
+console.log(Math.floor(bc));
+// The mathematical operator 'Math.floor' will always round a decimal up to
+//the nearest whole integer or actually to the nearest decicmal if there is more
+//than one decimal place
+
+Math.random()
+//The Math.random() function returns a floating-point, pseudo-random number in the range
+//[0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive), which you can then
+//scale to your desired range. The implementation selects the initial seed to the random
+//number generation algorithm; it cannot be chosen or reset by the user.
+
+//eamples
+// Returns a random number between 0 (inclusive) and 1 (exclusive)
+// function getRandom() {
+//   return Math.random();
+// }
+// // Returns a random number between min (inclusive) and max (exclusive)
+// function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+// // Returns a random integer between min (included) and max (excluded)
+// // Using Math.round() will give you a non-uniform distribution!
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+// // Returns a random integer between min (included) and max (included)
+// // Using Math.round() will give you a non-uniform distribution!
+// function getRandomIntInclusive(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(Math.random() * (11 - 1) + 1);
+
+var x = 10, y = -20;
+var z = Math.min(x, y);
+
+console.log(z);
+//Math.min() finds the minimum value of a set, is good for finding and setting boundaries
+
+var xx = 10, yy= -20;
+var zz = Math.max(xx, yy);
+
+console.log(zz);
+//Math.min() finds the maximum value of a set, is good for finding and setting boundaries

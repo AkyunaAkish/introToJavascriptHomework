f4a266640adb5b0dea1568bc9ea929317eec1239
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-08 01:13:26 -0700

    Added stretch goal

--- app.js
+++ app.js
@@ -24,3 +24,34 @@ console.log(multiplication);
 // The mathematical operator '/' is how javascript divides two values
 var division = 2 / 6;
 console.log(division);
+
+
+
+// Math.round rounds to nearest integer
+var x = Math.round(12.6);
+console.log(x);
+
+
+// Math.random returns random number between 0 and 1
+var y = Math.random();
+console.log(y);
+
+
+// Math.min returns lowest number
+var minimum = Math.min(1,5,8);
+console.log(minimum);
+
+
+// Math.max returns highest number
+var maximum = Math.max(1,5,8);
+console.log(maximum);
+
+
+// Math.floor rounds the number to the lower integer
+var z = Math.floor(3.8);
+console.log(z);
+
+
+// Math.ceil returns the lowest number that is greater or equal to the given number
+var top = Math.ceil(3.1);
+console.log(top);

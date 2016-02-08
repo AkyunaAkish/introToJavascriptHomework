42401cdf5e83ac76777c9ddc97f6c79437dc75a7
Brad Butterfield <butters5789@gmail.com>
2016-02-08 10:26:58 -0700

    practice with mathematical operators

---
+++ app.js
@@ -0,0 +1,45 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+// The mathematical operator Addition '+' is how javascript adds two values together
+var addition = 70 + 7;
+console.log("Addition " + addition);
+
+// The mathematical operator Subtration '-' is how javascript subtracts one value from another
+var subtraction = 50 - 45;
+console.log("Subtration " + subtraction);
+
+// The mathematical operator Division '/' is how javascript divides one value by another
+var division = 60 / 10;
+console.log("Division " + division);
+
+// The mathematical operator Multiplication '*' is how javascript multiplies one value by another
+var multiplication = 3 * 21;
+console.log("Multiplication " + multiplication);
+
+// The mathematical operator Remainder '%' is how javascript divides one number by another and tells you what is left over
+var remainder = 100 % 99;
+console.log("Remainder " + remainder);
+
+// The mathematical operator Increment '++' is how javascript adds one to the variable
+var x = 7;
+increment = ++x;
+console.log("Increment " + increment);
+
+// The mathematical operator Decrement -- is how javascript subtracts one to the variable
+var x = 11;
+decrement = --x;
+console.log("Decrement " + decrement);
+
+// The mathematical operator Unary negation '-' is how javascript turns values negative or opposite
+var x = 22;
+negation = -x;
+console.log("Negation " + negation);
+
+// The mathematical operator Unary plus '+' is how javascript turns values into numbers
+var x = "30";
+plus = +x;
+console.log("Plus " + plus);

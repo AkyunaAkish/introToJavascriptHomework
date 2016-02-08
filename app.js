1911295bdf4856b2cf5d95d9828452a8e2d6eb08
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-08 09:00:47 -0700

    Add math variables

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 var subtraction = 10 - 5
 
 console.log(subtraction)
+
+var division = 100 / 10
+
+console.log(division)
+
+var multiply = 7 * 3
+
+console.log(multiply)
+
+var remainder = 12 % 5
+
+console.log(remainder)
+
+var x = 5
+var increment = x++
+
+console.log(increment)
+
+var a = 10
+var decrement = a--
+
+console.log(decrement)
+
+var j = 5
+var negation = -j
+
+console.log(negation)
+
+var unaryplus = +"3"
+
+console.log(unaryplus)

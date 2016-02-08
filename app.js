e3d5ac5a23658862303e5b8008a6cad3a673ae19
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-08 00:43:36 -0700

    Mathmatical operators

---
+++ app.js
@@ -0,0 +1,26 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+
+// The mathematical operator '+' is how javascript adds two values together
+var addition = 3 + 3;
+console.log(addition);
+
+
+// The mathematical operator '-' is how javascript subtracts one value from another
+var subtraction = 2 - 2;
+console.log(subtraction);
+
+
+
+// The mathematical operator '*' is how javascript multiplies two values
+var multiplication = 2 * 6;
+console.log(multiplication);
+
+
+// The mathematical operator '/' is how javascript divides two values
+var division = 2 / 6;
+console.log(division);

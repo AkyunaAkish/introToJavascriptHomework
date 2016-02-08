b7c3b00a1168a4e6400d98b296b11e4f37c66e3b
Anthony Simpson <ansi6622@colorado.edu>
2016-02-08 09:06:20 -0700

    it may be perfect

--- app.js
+++ app.js
@@ -3,3 +3,43 @@ var b = 10;
 var c = a + b;
 
 console.log(c);
+
+// The mathematical operator '+' is how javascript adds two values together
+var addition = 2 + 2;
+console.log(addition);
+
+// The mathematical operator '-' is how javascript subtracts one value from another
+var subtraction = 5 - 4;
+console.log(subtraction);
+
+// The mathematical operator '/' is how javascript divides one value from another
+var division = 28 / 2;
+console.log(division);
+
+// The mathematical operator '*' is how javascript multiplies two values together
+var multiplication = 6 * 4;
+console.log(multiplication);
+
+// The mathematical operator '%' is how javascript takes the remainder of one value from another
+var remainder = 230 % 10;
+console.log(remainder);
+
+// The mathematical operator '++' is how javascript increments
+var increments = 2;
+x = increments++;
+console.log(x);
+
+// The mathematical operator '--' is how javascript decrements
+var decrements = 3;
+y = decrements--;
+console.log(y);
+
+// The mathematical operator '-' preceeding, (unary negation), is how javascript turns a number negative
+var negate = 5;
+z = -negate;
+console.log(z);
+
+// The mathematical operator '+' preceeding, (unary plus), is how javascript turns a number positive, or a non number into a number
+var plus = "malt";
+a = +plus;
+console.log(a);

25181aeb2b8e1625acc05492da7b677a071de248
Jake Vartanian <jake.vartanian@gmail.com>
2016-02-07 23:45:24 -0700

    all operators done

---
+++ app.js
@@ -0,0 +1,28 @@
+//this shows how expressions in js page can be executed in terminal
+
+var plus = 1+2
+console.log (plus)
+
+var minus = 2-1
+console.log (minus)
+
+var times = 2*3
+console.log (times)
+
+var divided = 18/2
+console.log (divided)
+
+var modulo = 9%4
+console.log (modulo)
+
+var great = 2 > 1
+console.log (great)
+
+var less = 2<1
+console.log (less)
+
+var equal = 2 == 1
+console.log (equal)
+
+var notequal = 2 != 1
+console.log (notequal)

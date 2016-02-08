df723fd99adb46f94a2738e21f1a9e90ffa99852
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-07 22:06:11 -0700

    initial commit with basic operators

---
+++ app.js
@@ -0,0 +1,50 @@
+var a = 22;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+// The mathematical operator '+' is how javascript adds two values together
+
+var d = 11;
+var e = 19
+var f = d - e;
+
+console.log(f);
+// The mathematical operator '-' is how javascript finds the difference btwn two values
+
+var g = 21;
+var h = 7;
+var i = (g/h);
+
+console.log(i);
+// The mathematical operator '/' is how javascript finds the quotient of two values
+
+var j = 3;
+var k = 42;
+var l = j*k;
+
+console.log(l);
+// The mathematical operator '*' is how javascript finds the product of two values
+
+
+var m = 10;
+var n = m++;
+
+console.log(n);
+// The mathematical operator '++' is how javascript increments a value by 1
+
+
+var o = 54;
+var p = o--;
+
+console.log(p);
+// The mathematical operator '--' is how javascript decrements a value by 1
+
+
+var aa = 20;
+var bb = 6;
+var cc = aa % bb;
+
+console.log(cc);
+// The mathematical operator '%' (modulo )is how javascript finds the integer remainder
+// of two values

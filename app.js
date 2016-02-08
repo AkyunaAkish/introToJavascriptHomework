241eaf1741241275103d24a37649c7ad00072528
Madeline Pickering <madelinepickering@gmail.com>
2016-02-07 19:54:07 -0700

    Stretch goals added

--- app.js
+++ app.js
@@ -39,3 +39,25 @@ console.log(h);
 // operator '+' ahead of a variable attempts to convert it to a number
 var strang = "7"
 console.log(+strang);
+
+// Math.round returns a rounded number to the nearest integer
+var round = 3.24159;
+console.log(Math.round(round));
+
+// Math.random returns a random number between 0 and 1
+console.log(Math.random());
+
+// Math.min returns the lowest number of a set of values
+var newVar = 1;
+var anotherVar = 7;
+console.log(Math.min(newVar, anotherVar));
+
+// Math.max returns the largest number of a set of values
+console.log(Math.max(newVar, anotherVar));
+
+// Math.floor returns the integer less than or equal to a value
+var hi = 39.993;
+console.log(Math.floor(hi));
+
+// Math.ceil returns the integer greater than or equal to a value
+console.log(Math.ceil(hi));
